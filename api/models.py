6dc81eaf92c2992f7689753b0035733170fa9830
João Neves <joao.neves@enecad.com.br>
2020-06-23 18:32:23 -0300

    first submit

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
-from django.core.validators import MinLengthValidator, EmailValidator, validate_ipv4_address
+from django.core.validators import MinLengthValidator, EmailValidator
+from django.core.validators import validate_ipv4_address
 from django.db import models
 import datetime
 
@@ -41,7 +42,10 @@ class User(models.Model):
 class Agent(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
-    address = models.GenericIPAddressField(validators=[validate_ipv4_address], null=True)
+    address = models.GenericIPAddressField(
+        validators=[validate_ipv4_address],
+        null=True
+    )
     status = models.BooleanField(default=False)
     env = models.CharField(max_length=20)
     version = models.CharField(max_length=5)
@@ -61,8 +65,7 @@ class Event(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.level +' in ' + self.agent.name
+        return self.level + ' in ' + self.agent.name
 
     class Meta:
         ordering = ['date']
-
--- main.py
+++ main.py
@@ -1,36 +1,66 @@
-from api.models import User, Agent, Event, Group
+from api.models import User, Agent, Event
+from datetime import datetime, timedelta
 
 
 def get_active_users() -> User:
-    """Traga todos os uarios ativos, seu último login deve ser menor que 10 dias """
-    raise NotImplementedError
+    users = User.objects.all()
+    date_back_10_days = datetime.now() - timedelta(days=10)
+    filtered_users = users.filter(last_login__gt=date_back_10_days)
+    return filtered_users
 
 
-def get_amount_users() -> User:
-    """Retorne a quantidade total de usuarios do sistema """
-    raise NotImplementedError
+def get_amount_users():
+    users = User.objects.all()
+    count_users = len(users)
+    return count_users
 
 
 def get_admin_users() -> User:
-    """Traga todos os usuarios com grupo = 'admin"""
-    raise NotImplementedError
+    users = User.objects.all()
+    filtered_users = users.filter(group__name='admin')
+    return filtered_users
 
 
 def get_all_debug_events() -> Event:
-    """Traga todos os eventos com tipo debug"""
-    raise NotImplementedError
+    events = Event.objects.all()
+    filtered_events = events.filter(level__exact='debug')
+    return filtered_events
 
 
 def get_all_critical_events_by_user(agent) -> Event:
-    """Traga todos os eventos do tipo critico de um usuário específico"""
-    raise NotImplementedError
+    events = Event.objects.all()
+    events_with_level_critico = events.filter(level__exact='critico')
+
+    agent_id = agent.objects.values('id')
+
+    events_by_agent = events_with_level_critico.filter(
+        agent_id__exact=agent_id)
+    return events_by_agent
 
 
 def get_all_agents_by_user(username) -> Agent:
-    """Traga todos os agentes de associados a um usuário pelo nome do usuário"""
-    raise NotImplementedError
+    agents = Agent.objects.all()
+    filtered_agents = agents.filter(user__name=username)
+    return filtered_agents
 
 
-def get_all_events_by_group() -> Group:
-    """Traga todos os grupos que contenham alguem que possua um agente que possuem eventos do tipo information"""
-    raise NotImplementedError
+def get_all_events_by_group():
+    events = Event.objects.all()
+    information_events = events.filter(level__exact='information')
+
+    fk_agents_ids = []
+    for event in information_events:
+        fks = information_events.values('agent_id')
+        fk_agents_ids.append(fks)
+
+    users_ids = []
+    for ident in fk_agents_ids:
+        agent_by_id = Agent.objects.filter(id__exact=ident)
+        userid_on_agent = agent_by_id.values('user_id')
+        users_ids.append(userid_on_agent)
+
+    users = []
+    for each_id in users_ids:
+        users = User.objects.filter(id__exact=each_id)
+
+    return users
